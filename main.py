a20819b668da2d02edb733b95a72c857f6e2a656
And5reas <And5reas.porcel@hotmail.com>
2024-04-30 14:57:00 -0300

    ...

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ nail_ids = [0, 4, 8, 12, 16, 20]
 ser = serial.Serial('COM3', 9600)
 
 def cal_dist_euclid(p1, p2):
-    return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
+    return True if math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2) < 0.2 else False
 
 cap = cv.VideoCapture(1)
 with mpHand.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5) as hand:
@@ -27,31 +27,34 @@ with mpHand.Hands(static_image_mode=False, max_num_hands=1, min_detection_confid
         if res_hand.multi_hand_landmarks:
             for handLms in res_hand.multi_hand_landmarks:
 
+                h, w, c = img.shape
+
+                pulso = (handLms.landmark[0].x, handLms.landmark[0].y)
                 polegar = (handLms.landmark[4].x, handLms.landmark[4].y)
                 indicador = (handLms.landmark[8].x, handLms.landmark[8].y)
 
-                close_hand = True if (cal_dist_euclid(polegar, indicador) < 0.2) else False
+                cv.circle(img, (int(pulso[0]*w) , int(pulso[1]*h)), 15, (255, 0, 255), cv.FILLED)
+                cv.circle(img, (int(polegar[0]*w) , int(polegar[1]*h)), 15, (0, 255, 255), cv.FILLED)
+                cv.circle(img, (int(indicador[0]*w) , int(indicador[1]*h)), 15, (255, 255, 0), cv.FILLED)
 
-                for id, lm in enumerate (handLms.landmark):
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x*w), int(lm.y*h)
-                    if id in nail_ids:
-                        if (id == 4):
-                            cv.circle(img, (cx,cy), 15, (0, 255, 255),cv.FILLED)
-                        elif (id == 0):
-                            if (not close_hand):
-                                if (cx > w/2 + 50):
-                                    ser.write("1h".encode())
-                                elif (cx < w/2 - 50):
-                                    ser.write("2h".encode())
-                                else:
-                                    ser.write("SS".encode())
-                            else:
-                                ser.write("SS".encode())
+                if (cal_dist_euclid(polegar, indicador) < 0.2):
+                    ser.write("SS".encode())
+                    if int(pulso[0]*w) > ((w/2) + 50):
+                        ser.write("4h".encode())
+                    elif int(pulso[0]*w) < ((w/2) - 50):
+                        ser.write("4a".encode())
+                    else:
+                        ser.write("4s".encode())
+                else:
+                    if int(pulso[1]*h) > ((h/2) + 50):
+                        ser.write("1h".encode())
+                    elif int(pulso[1]*h) < ((h/2) - 50):
+                        ser.write("1a".encode())
+                    else:
+                        ser.write("1s".encode())
+                    ser.write("4s".encode())
+                    print("Mão fechada")
 
-                            cv.circle(img,(cx,cy),15, (255, 255, 0),cv.FILLED)
-                        else:
-                            cv.circle(img,(cx,cy),15, (255, 0, 255),cv.FILLED)
                 lapis.draw_landmarks(img, handLms, mpHand.HAND_CONNECTIONS)
         else:
             ser.write("SS".encode())
